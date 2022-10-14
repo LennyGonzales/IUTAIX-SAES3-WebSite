55ea14e3f9ae7d2951baac6a6e8aecce0770e408
AlexandreGanassi <ganassialex@gmail.com>
2022-10-14 15:10:09 +0200

    vid et texte accueil

--- Home/mainHome.php
+++ Home/mainHome.php
@@ -1,1 +1,17 @@
-<?php
+<html>
+<head>
+    <meta content="text/html; charset=ISO-8859-1"
+          http-equiv="content-type">
+    <title>Site web de l'app</title>
+</head>
+<body>
+        <video src="../Video/vidTest.mp4"
+               type="video/mp4" controls="" poster="">
+        </video>
+<br>
+<br>
+</body>
+</html>
+
+
+<?php echo "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. "?>
