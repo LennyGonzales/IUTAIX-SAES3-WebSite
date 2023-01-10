84b53368a8bf499108af1b0e71a5af55ed811cc5
MohammedKaddouri <98416541+MohammedKaddouri@users.noreply.github.com>
2023-01-10 10:32:09 +0100

    Add files via upload

--- Php/history.php
+++ Php/history.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../Css/scenario-style.css">
         <title>Modifications/ajouter des questions</title>
     </head>
     <body>
