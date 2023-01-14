666a2d442bc974f7f02cfa640ea75654ca6d6c7b
MohammedKaddouri <98416541+MohammedKaddouri@users.noreply.github.com>
2023-01-14 18:09:47 +0100

    Add files via upload

--- Php/pages/historyPage.php
+++ Php/pages/historyPage.php
@@ -32,6 +32,28 @@
         require("../connectionSQL.php");
         ?>
 
+        <h1>Formulaire d'ajout questions à réponse écrite</h1>
+        <form action="../addQuestion.php" method="post">
+            Module: <input type="text" name="module"><br>
+            Description: <input type="text" name="description"><br>
+            Question: <input type="text" name="question"><br>
+            Réponse vraie Ecrite: <input type="text" name="true_answer"><br>
+            <input type="submit" name="add" value="Ajouter">
+        </form>
+
+        <br>
+        <h1>Formulaire d'ajout questions à choix multiples</h1>
+        <form action="../addQuestion.php" method="post">
+            Module: <input type="text" name="module"><br>
+            Description: <input type="text" name="description"><br>
+            Question: <input type="text" name="question"><br>
+            Réponse vraie <input type="number" name="true_answer" max="3" min="1"><br>
+            Réponse 1: <input type="text" name="answer_1"><br>
+            Réponse 2: <input type="text" name="answer_2"><br>
+            Réponse 3: <input type="text" name="answer_3"><br>
+            <input type="submit" name="add" value="Ajouter">
+        </form>
+
         <div class="tabla-responsive">
             <table class="table-bordered">
                 <thead>
@@ -82,27 +104,6 @@
         require "../ShowUpdateFormQuestion.php";
     } else {
 ?>
-        <h1>Formulaire d'ajout questions à réponse écrite</h1>
-        <form action="../addQuestion.php" method="post">
-            Module: <input type="text" name="module"><br>
-            Description: <input type="text" name="description"><br>
-            Question: <input type="text" name="question"><br>
-            Réponse vraie Ecrite: <input type="text" name="true_answer"><br>
-            <input type="submit" name="add" value="Ajouter">
-        </form>
-
-        <br>
-        <h1>Formulaire d'ajout questions à choix multiples</h1>
-        <form action="../addQuestion.php" method="post">
-            Module: <input type="text" name="module"><br>
-            Description: <input type="text" name="description"><br>
-            Question: <input type="text" name="question"><br>
-            Réponse vraie <input type="number" name="true_answer" max="3" min="1"><br>
-            Réponse 1: <input type="text" name="answer_1"><br>
-            Réponse 2: <input type="text" name="answer_2"><br>
-            Réponse 3: <input type="text" name="answer_3"><br>
-            <input type="submit" name="add" value="Ajouter">
-        </form>
 <?php
 }
 ?>
