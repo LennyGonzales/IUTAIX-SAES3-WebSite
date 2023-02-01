9642b52a51283331119ceea4a023945f21e27441
LennyGonzales <lenny.gonzales@etu.univ-amu.fr>
2023-02-02 00:43:27 +0100

    The user can create an account without verification

--- Model/Users.php
+++ Model/Users.php
@@ -7,39 +7,60 @@
  */
 class Users extends Model {
 
-    public static function createAccount(Array $A_parameters):array {
-        $email = $A_parameters['email'];
-        $password = $A_parameters['user_password'];
+    const DATABASE = 'USERS';
 
-        $messageType = 'error';
-        if(self::checkIfExistsByPrimaryKey('EMAIL', $email)) { // Verify if the user already exists
+    public static function checkIfExistsByEmail(string $S_email = null):bool {
+        $P_db = Connection::initConnection(self::DATABASE);
+        $S_stmnt = "SELECT * FROM USERS WHERE EMAIL = :email ";
+        $P_sth = $P_db->prepare($S_stmnt);
+        $P_sth->bindValue(':email', $S_email, PDO::PARAM_STR);
+        $P_sth->execute();
+        return ($P_sth->rowCount() > 0);
+    }
+
+    public static function create(array $A_values = null):bool {
+        $P_db = Connection::initConnection(self::DATABASE);
+        $S_stmnt = "INSERT INTO USERS (EMAIL,USER_PASSWORD) VALUES (:email, :user_password)";
+        $P_sth = $P_db->prepare($S_stmnt);
+        $P_sth->bindValue(':email', $A_values['email'], PDO::PARAM_STR);
+        $P_sth->bindValue(':user_password', $A_values['user_password'], PDO::PARAM_STR);
+        $B_state = $P_sth->execute();
+        return $B_state;
+    }
+
+    public static function createAccount(Array $A_parameters):array {
+        $S_email = $A_parameters['email'];
+        $S_password = $A_parameters['user_password'];
+
+        $S_messageType = 'error';
+        if(self::checkIfExistsByEmail($S_email)) { // Verify if the user already exists
             return array(
-                'messageType' => $messageType,
+                'messageType' => $S_messageType,
                 'message' => "L'utilisateur est déjà existant, veuillez-vous connecter"
             );
         }
 
         // Verification of the email and the password
-        if (substr($email, -16, 16) == "@etu.univ-amu.fr" || $email == "safa.yahi@univ-amu.fr") {
+        if (substr($S_email, -16, 16) == "@etu.univ-amu.fr" || $S_email == "safa.yahi@univ-amu.fr") {
             // Vérifie si le mot de passe contient 12 caractères, au moins une majuscule,un caractère spécial et un chiffre
-            if (strlen($password) < 12) {
+            if (strlen($S_password) < 12) {
                 return array(
-                    'messageType' => $messageType,
+                    'messageType' => $S_messageType,
                     'message' => 'Le mot de passe doit comporter 12 caractères.'
                 );
-            } elseif (!preg_match('/[A-Z]/', $password)) {
+            } elseif (!preg_match('/[A-Z]/', $S_password)) {
                 return array(
-                    'messageType' => $messageType,
+                    'messageType' => $S_messageType,
                     'message' => 'Le mot de passe doit contenir au moins une majuscule.'
                 );
-            } elseif (!preg_match('/[\'^£$%&*()}{@#~?!><>,;.|=_+¬-]/', $password)) {
+            } elseif (!preg_match('/[\'^£$%&*()}{@#~?!><>,;.|=_+¬-]/', $S_password)) {
                 return array(
-                    'messageType' => $messageType,
+                    'messageType' => $S_messageType,
                     'message' => 'Le mot de passe doit contenir un caractère spécial.'
                 );
-            } elseif (!preg_match('/\d/', $password)) {
+            } elseif (!preg_match('/\d/', $S_password)) {
                 return array(
-                    'messageType' => $messageType,
+                    'messageType' => $S_messageType,
                     'message' => 'Le mot de passe doit contenir au moins un chiffre.'
                 );
             }
@@ -47,7 +68,7 @@ class Users extends Model {
             $A_parameters['user_password'] = hash('sha512', $A_parameters['user_password']);
             if(!self::create($A_parameters)) {
                 return array(
-                    'messageType' => $messageType,
+                    'messageType' => $S_messageType,
                     'message' => 'Erreur du serveur, veuillez réessayer.'
                 );
             }
@@ -59,7 +80,7 @@ class Users extends Model {
         }
 
         return array(
-            'messageType' => $messageType,
+            'messageType' => $S_messageType,
             'message' => "L'email inséré n'est pas un email AMU."
         );
     }
@@ -80,7 +101,7 @@ class Users extends Model {
     }
 
     public static function selectStatus(Array $A_parameters):?string {
-        $P_db = Connection::initConnection();
+        $P_db = Connection::initConnection(self::DATABASE);
         $S_stmnt = "SELECT USER_STATUS FROM USERS WHERE EMAIL = :email";
         $P_sth = $P_db->prepare($S_stmnt);
         $P_sth->bindValue(':email', $A_parameters['email'], PDO::PARAM_INT);
